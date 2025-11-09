7fdfaf88aa283e4638fa4b527dadb2779fe543db
Subroto Kumar <152253595+SubrotoKumar7@users.noreply.github.com>
2025-11-09 14:40:43 +0600

    navbar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,59 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router';
 
 const Navbar = () => {
+    const links = <>
+        <li><NavLink to={'/'}>Home</NavLink></li>
+        <li><NavLink to={'/'}>All Property</NavLink></li>
+        <li><NavLink to={'/'}>Add Property</NavLink></li>
+        <li><NavLink to={'/'}>My Ratings</NavLink></li>
+    </>
     return (
-        <nav>
-            
+        <nav className='shadow-sm bg-base-100'>
+            <div className="w-11/12 mx-auto navbar">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                    </div>
+                    <ul
+                        tabIndex="-1"
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                        {links}
+                    </ul>
+                    </div>
+                    <Link to={'/'} className='text-4xl font-bold'>Nestora</Link>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                    {links}
+                    </ul>
+                </div>
+                <div className="navbar-end flex items-center gap-3">
+                    <div className="dropdown dropdown-end">
+                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                            <div className="w-10 rounded-full">
+                            <img
+                                alt="Tailwind CSS Navbar component"
+                                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                            </div>
+                        </div>
+                        <ul
+                            tabIndex="-1"
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                            <li>
+                            <a className="justify-between">
+                                Profile
+                                <span className="badge">New</span>
+                            </a>
+                            </li>
+                            <li><a>Settings</a></li>
+                            <li><a>Logout</a></li>
+                        </ul>
+                    </div>
+                    <a className="btn">Login</a>
+                </div>
+            </div>
         </nav>
     );
 };
