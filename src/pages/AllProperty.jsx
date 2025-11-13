7e5b28ef4f34ef4514536fc5e313099309457fb0
Subroto Kumar <152253595+SubrotoKumar7@users.noreply.github.com>
2025-11-13 23:04:16 +0600

    search method added

--- src/pages/AllProperty.jsx
+++ src/pages/AllProperty.jsx
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router';
 import Card from '../components/Card';
+import { FiRefreshCw } from 'react-icons/fi';
 
 const AllProperty = () => {
     const propertiesData = useLoaderData();
+    const [search, setSearch] = useState('');
+
+
+    const filterProperty = propertiesData.filter((property) =>
+        property.name.toLowerCase().includes(search.toLowerCase()) ||
+        property.description.toLowerCase().includes(search.toLowerCase())
+    );
+    const reset = () => { setSearch('');  };
+
     return (
         <div className='w-11/12 mx-auto sec-gap'>
             <div className='text-center space-y-2 mb-10'>
                 <h1 className='heading font-heading'>Explore Exclusive Real Estate Listings</h1>
                 <p>Handpicked properties with premium amenities, modern architecture, and unbeatable locations — find your next investment today.</p>
             </div>
+            <div>
+                <div className="relative m-2 text-center w-full sm:w-auto md:px-20 flex justify-center sm:justify-end">
+                <input
+                    type="text"
+                    placeholder="Search Properties ....."
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                    className="w-[80%] mx-auto sm:w-62 md:w-86 px-6 py-2 pr-10 rounded border border-gray-500 focus:outline-none focus:ring-2 focus:ring-black"
+                    />                    
+                </div>
+            </div>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 mt-7'>
                 {
-                    propertiesData.map(property => <Card key={property._id} property={property}></Card>)
+                    
+                    filterProperty.map(property => <Card key={property._id} property={property}></Card>)
                 }
             </div>
         </div>
