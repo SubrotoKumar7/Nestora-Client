abc2715587771f600bf3d4d127056e93c6c8c8e1
Subroto Kumar <152253595+SubrotoKumar7@users.noreply.github.com>
2025-11-15 00:47:28 +0600

    sort method added

--- src/pages/AllProperty.jsx
+++ src/pages/AllProperty.jsx
@@ -6,6 +6,7 @@ const AllProperty = () => {
     const propertiesData = useLoaderData();
     const [search, setSearch] = useState('');
     const [properties, setProperties] = useState(propertiesData);
+    const [sort, setSort] = useState('');
 
     useEffect(()=> {
         const filterProperty = propertiesData && propertiesData.length > 0
@@ -17,6 +18,13 @@ const AllProperty = () => {
         setProperties(filterProperty);
     }, [propertiesData, search])
 
+    useEffect(()=> {
+        fetch(`http://localhost:3000/sort?price=${sort}`)
+        .then(res=> res.json())
+        .then(data=> {
+            setProperties(data);
+        })
+    }, [sort])
 
     return (
         <div className='w-11/12 mx-auto sec-gap'>
@@ -24,20 +32,26 @@ const AllProperty = () => {
                 <h1 className='heading font-heading'>Explore Exclusive Real Estate Listings</h1>
                 <p>Handpicked properties with premium amenities, modern architecture, and unbeatable locations — find your next investment today.</p>
             </div>
-            <div>
-                <div className="relative m-2 text-center w-full sm:w-auto md:px-20 flex justify-center sm:justify-end">
-                <input
-                    type="text"
-                    placeholder="Search Properties ....."
-                    value={search}
-                    onChange={(e) => setSearch(e.target.value)}
-                    className="w-[80%] mx-auto sm:w-62 md:w-86 px-6 py-2 pr-10 rounded border border-gray-500 focus:outline-none focus:ring-2 focus:ring-black"
+            <div className="flex flex-col md:flex-row justify-between items-center gap-5">
+                <div>
+                    <input
+                        type="text"
+                        placeholder="Search Properties ....."
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                        className="w-[80%] mx-auto sm:w-62 md:w-86 px-6 py-2 pr-10 rounded border border-gray-500 focus:outline-none focus:ring-2 focus:ring-black"
                     />                    
                 </div>
+                <fieldset className="fieldset">
+                    <select onChange={(e)=> setSort(e.target.value)} defaultValue="sort by" className="select">
+                        <option value={'none'}>Default Sort</option>
+                        <option value={'low2high'}>Low to High (Price)</option>
+                        <option value={'high2low'}>High to Low (Price)</option>
+                    </select>
+                </fieldset>
             </div>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 mt-7'>
-                {
-                    
+                {                    
                     properties.map(property => <Card key={property._id} property={property}></Card>)
                 }
             </div>
