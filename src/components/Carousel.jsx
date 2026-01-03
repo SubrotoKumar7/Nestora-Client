a1b24d357320334a03717cc8d7d5eeb2e85c3fb7
Subroto Kumar <152253595+SubrotoKumar7@users.noreply.github.com>
2026-01-04 00:17:07 +0600

    carousel update

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -6,6 +6,7 @@ import 'swiper/css/autoplay';
 import slide1 from '../assets/slide1.jpg';
 import slide2 from '../assets/slide2.jpg';
 import slide3 from '../assets/slide3.jpg';
+import { Link } from 'react-router';
 
 const Carousel = () => {
 
@@ -32,17 +33,18 @@ const sliderData = [
 
     
     return (
-        <div className='relative h-screen'>
+        <div className='relative'>
             <Swiper autoplay={{delay: 3000}} speed={1000} loop effect='fade'  modules={[Autoplay]}>
                 {
                     sliderData.map(slide => 
                     <SwiperSlide key={slide.id}>
-                        <div className='relative h-screen text-center'>
+                        <div className='relative h-[90vh] text-center'>
                             <img className='absolute w-full h-full' src={slide.image} alt="slider images" />
                             <div className='absolute w-full h-full z-10 bg-black/40'></div>
                             <div className='absolute w-full h-full text-white z-20 top-[45%]'>
                                 <h1 className='text-center text-4xl md:text-6xl font-bold'>{slide.title}</h1>
                                 <p className='mt-3 text-lg'>{slide.subtitle}</p>
+                                <Link className='btn btn-primary mt-3' to={'/all-property'}>Explore Property</Link>
                             </div>
                         </div>
                     </SwiperSlide>)
