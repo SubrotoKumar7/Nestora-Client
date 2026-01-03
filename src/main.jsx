ec9e6227bba1fed98f2d0fb2f9ff773b9ae174bb
Subroto Kumar <152253595+SubrotoKumar7@users.noreply.github.com>
2026-01-03 22:53:12 +0600

    tanstack query setup

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,17 @@ import router from './routes/router'
 import AuthProvider from './context/AuthProvider'
 import { ToastContainer, Zoom } from 'react-toastify'
 import '@smastrom/react-rating/style.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router}></RouterProvider>
-      <ToastContainer position="top-center" autoClose={2000} transition={Zoom}/>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router}></RouterProvider>
+        <ToastContainer position="top-center" autoClose={2000} transition={Zoom}/>
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>,
 )
